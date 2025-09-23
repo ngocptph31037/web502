91443bc866b2e42c61737b1610691f32e3211e7b
ngocptph31037 <pngoc2136@gmail.com>
2025-09-23 19:09:04 +0700

    Thêm thư mục web502 vào nhánh riêng

---
+++ web502/web502/src/dientich.ts
@@ -0,0 +1,8 @@
+let a: number =5;
+let b: number =10;
+const dienTich = a*b;
+return {dientich};
+const chuVi = 2*(a+b);
+return {chuVi};
+console.log("Chu vi:", result.chuVi);
+console.log("Diện tích:", result.dientich);
---
+++ web502/web502/src/soNguyenTo.ts
@@ -0,0 +1,9 @@
+function isPrime(n: number): boolean {
+  if (n < 2) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+console.log(isPrime(7));
+console.log(isPrime(10)); 
